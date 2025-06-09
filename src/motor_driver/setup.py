ca91cce76ccb1feb50d482f5d792faa5565516e2
Tushar_Chillal <80667954+TusharChillal@users.noreply.github.com>
2025-06-09 22:44:09 +0530

    setup.py

--- src/motor_driver/setup.py
+++ src/motor_driver/setup.py
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'manual_motor_driver = motor_driver.manual_motor_driver:main',
-            'twist_motor_driver = motor_driver.twist_motor_driver:main',
+            
         ],
     },
 )
