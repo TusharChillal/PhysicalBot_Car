af12c08b0c19978d61d1d281c84ca4724c4bab76
Tushar_Chillal <80667954+TusharChillal@users.noreply.github.com>
2025-06-17 19:49:28 +0530

    setup.py

--- motor_driver/setup.py
+++ motor_driver/setup.py
@@ -9,19 +9,23 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/joy_motor_driver_launch.xml']),
+        ('share/' + package_name + '/launch', [
+            'launch/joy_motor_driver_launch.py',
+            'launch/joy_motor_driver_servo_launch.py'
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tusharchillal',
     maintainer_email='chillal.tushar@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Motor driver ROS 2 package for joystick and servo control',
+    license='MIT',  # ← Update this if you're using a different license
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'twist_motor_driver = motor_driver.twist_motor_driver:main',
             'joy_motor_driver = motor_driver.joy_motor_driver:main',
+            'joy_motor_driver_servo = motor_driver.joy_motor_driver_servo:main',
         ],
     },
 )
